379704c37da3c72c6421e540c2bce968d601b088
Anton Vakhrushev <anwinged@ya.ru>
2016-02-16 15:58:01 +0300

    Add color key to TbAlert

--- widgets/TbAlert.php
+++ widgets/TbAlert.php
@@ -1,66 +1,72 @@
 <?php
+
 /**
  * TbAlert class file.
+ *
  * @author Christoffer Niska <christoffer.niska@gmail.com>
  * @copyright Copyright &copy; Christoffer Niska 2013-
  * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
- * @package bootstrap.widgets
  */
 
 /**
  * Bootstrap alert widget.
+ *
  * @see http://twitter.github.com/bootstrap/javascript.html#alerts
  */
 class TbAlert extends CWidget
 {
     /**
+     * Alert configuration.
+     *
+     * You can set up `visible`, `color`, `htmlOptions` per alert apart from widget-level properties.
+     *
      * @var array the alerts configurations (style=>config).
      */
     public $alerts;
     /**
-     * @var string|boolean the close link text. If this is set false, no close link will be displayed.
+     * @var string|bool the close link text. If this is set false, no close link will be displayed.
      */
     public $closeText = TbHtml::CLOSE_TEXT;
     /**
-     * @var boolean indicates whether the alert should be an alert block. Defaults to 'true'.
+     * @var bool indicates whether the alert should be an alert block. Defaults to 'true'.
      */
     public $block = true;
     /**
-     * @var boolean indicates whether alerts should use transitions. Defaults to 'true'.
+     * @var bool indicates whether alerts should use transitions. Defaults to 'true'.
      */
     public $fade = true;
     /**
      * @var string[] the JavaScript event configuration (name=>handler).
      */
-    public $events = array();
+    public $events = [];
     /**
      * @var array the HTML attributes for the alert container.
      */
-    public $htmlOptions = array();
+    public $htmlOptions = [];
 
     /**
      * Initializes the widget.
      */
     public function init()
     {
-        $this->attachBehavior('TbWidget', new TbWidget);
+        $this->attachBehavior('TbWidget', new TbWidget());
         $this->copyId();
         if (is_string($this->alerts)) {
-            $colors = explode(' ', $this->alerts);
+            $keys = explode(' ', $this->alerts);
         } else {
             if (!isset($this->alerts)) {
-                $colors = array(
+                $keys = [
                     TbHtml::ALERT_COLOR_SUCCESS,
                     TbHtml::ALERT_COLOR_WARNING,
                     TbHtml::ALERT_COLOR_INFO,
                     TbHtml::ALERT_COLOR_DANGER,
-                ); // render all styles by default
+                ]; // render all styles by default
             }
         }
-        if (isset($colors)) {
-            $this->alerts = array();
-            foreach ($colors as $color) {
-                $this->alerts[$color] = array();
+        if (isset($keys)) {
+            $this->alerts = [];
+            foreach ($keys as $key) {
+                $this->alerts[$key] = [];
             }
         }
     }
@@ -76,17 +82,18 @@ class TbAlert extends CWidget
             return;
         }
         echo TbHtml::openTag('div', $this->htmlOptions);
-        foreach ($this->alerts as $color => $alert) {
+        foreach ($this->alerts as $key => $alert) {
             if (isset($alert['visible']) && !$alert['visible']) {
                 continue;
             }
 
-            if ($user->hasFlash($color)) {
-                $htmlOptions = TbArray::popValue('htmlOptions', $alert, array());
+            if ($user->hasFlash($key)) {
+                $htmlOptions = TbArray::popValue('htmlOptions', $alert, []);
                 TbArray::defaultValue('closeText', $this->closeText, $htmlOptions);
                 TbArray::defaultValue('block', $this->block, $htmlOptions);
                 TbArray::defaultValue('fade', $this->fade, $htmlOptions);
-                echo TbHtml::alert($color, $user->getFlash($color), $htmlOptions);
+                $color = TbArray::getValue('color', $alert, $key);
+                echo TbHtml::alert($color, $user->getFlash($key), $htmlOptions);
             }
         }
         echo '</div>';
